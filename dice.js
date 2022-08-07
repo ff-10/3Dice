2377cc6404477582de5bf2ae95563129a1461304
Fatali Fataliyev <fatali.fataliiyev@gmail.com>
2022-08-07 04:31:23 +0400

    Optimized useless codes deleted

--- dice.js
+++ dice.js
@@ -1,29 +1,15 @@
 (function () {
+	//define variables
+	let rotateY = 0, rotateX = 0;
 
-	//define values
-	var rotateY = 0,
-	 	  rotateX = 0;
-
-    // keydown eventhandler
+    		// keydowns
 	 	document.onkeydown = function (e) {
-
 	 		if (e.keyCode === 37/*LEFT*/) rotateY -= 5
 	 		else if (e.keyCode === 38/*TOP*/) rotateX += 5
 	 		else if (e.keyCode === 39/*RIGHT*/) rotateY += 5
 	 		else if (e.keyCode === 40/*BOTTOM*/) rotateX -= 5
 
-	 		//Add degrees to transform:;
-	 		document.querySelector('.dice').style.transform =
-	 		'rotateY(' + rotateY + 'deg)'+'rotateX(' + rotateX + 'deg)';
-
+	 		//Add degrees to transform:
+	 		document.querySelector('.dice').style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
 	 	}
 })();
-
-let dice = document.querySelector('.dice');
-
-function auto__mode__on() {
-  dice.style.animationName = "auto_rotate";
-}
-function auto__mode__off(){
-  dice.style.animationName = "";
-}
